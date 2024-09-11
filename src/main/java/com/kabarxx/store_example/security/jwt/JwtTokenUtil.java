134d70e178fb5c7e161f4b5001c6a865a75daec7
LENOVO <eldar200715@gmail.com>
2024-09-11 15:58:12 +0300

    create method getUsernameFromToken

--- src/main/java/com/kabarxx/store_example/security/jwt/JwtTokenUtil.java
+++ src/main/java/com/kabarxx/store_example/security/jwt/JwtTokenUtil.java
@@ -1,6 +1,7 @@
 package com.kabarxx.store_example.security.jwt;
 
 import com.kabarxx.store_example.domain.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,14 +18,30 @@ public class JwtTokenUtil {
     @Value("{secret.key}")
     private final String SECRET = "RINAZARETSKAYAMOYALUBOVNAVCUJIZN";
 
+    private SecretKey getSecretKey() {
+        return new SecretKeySpec(Base64.getDecoder().decode(SECRET), SignatureAlgorithm.HS256.getJcaName());
+    }
+
     public String generateToken(User user) {
-        SecretKey key = new SecretKeySpec(Base64.getDecoder().decode(SECRET), SignatureAlgorithm.HS256.getJcaName());
+        SecretKey secretKey = getSecretKey();
 
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    public Claims getUsernameFromToken(String token) {
+        SecretKey secretKey = getSecretKey();
+
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
 }
