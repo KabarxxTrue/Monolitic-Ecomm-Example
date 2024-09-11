d4283cc744d435a1dd7aaa5007935e3b0db31660
LENOVO <eldar200715@gmail.com>
2024-09-11 15:31:16 +0300

    some small edits for dto's and mapper

--- src/main/java/com/kabarxx/store_example/domain/dto/user/UserLoginDTO.java
+++ src/main/java/com/kabarxx/store_example/domain/dto/user/UserLoginDTO.java
@@ -1,5 +1,6 @@
 package com.kabarxx.store_example.domain.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserLoginDTO
 {
     private String username;
--- src/main/java/com/kabarxx/store_example/mappers/UserMapper.java
+++ src/main/java/com/kabarxx/store_example/mappers/UserMapper.java
@@ -4,11 +4,13 @@ import com.kabarxx.store_example.domain.User;
 import com.kabarxx.store_example.domain.dto.user.UserDTO;
 import com.kabarxx.store_example.domain.dto.user.UserRegistrationDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper
 {
     User toEntity(UserRegistrationDTO userRegistrationDTO);
 
+    @Mapping(target = "password", ignore = true)
     UserDTO toDTO(User user);
 }
