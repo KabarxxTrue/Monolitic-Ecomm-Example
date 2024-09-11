c471b76d7ec7e19e2c4fa1a8387e64062796295c
LENOVO <eldar200715@gmail.com>
2024-09-11 14:23:21 +0300

    Initial commit

---
+++ src/main/java/com/kabarxx/store_example/StoreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kabarxx.store_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreExampleApplication.class, args);
+	}
+
+}
