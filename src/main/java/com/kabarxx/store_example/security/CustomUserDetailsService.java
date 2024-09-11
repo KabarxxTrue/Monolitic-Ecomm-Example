64ac64a16f65a5cd5ae4ecaf2978f9afd115ec9a
LENOVO <eldar200715@gmail.com>
2024-09-11 16:11:05 +0300

    realized user details

--- src/main/java/com/kabarxx/store_example/security/CustomUserDetailsService.java
+++ src/main/java/com/kabarxx/store_example/security/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.kabarxx.store_example.security;
 
-public class CustomUserDetailsService {
+import com.kabarxx.store_example.domain.User;
+import com.kabarxx.store_example.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
 }
