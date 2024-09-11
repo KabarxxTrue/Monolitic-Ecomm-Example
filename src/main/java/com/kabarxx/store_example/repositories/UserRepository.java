1eed290bdbd1ab13f05ac1e7c7ac7361ddbf88c9
LENOVO <eldar200715@gmail.com>
2024-09-11 15:34:10 +0300

    added methods for find by username and for checks

--- src/main/java/com/kabarxx/store_example/repositories/UserRepository.java
+++ src/main/java/com/kabarxx/store_example/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.kabarxx.store_example.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsernameOrEmail(String username, String email);
 }
