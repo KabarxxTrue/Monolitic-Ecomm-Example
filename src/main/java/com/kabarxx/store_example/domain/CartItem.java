a53c5509f8f0ec15f958455158feaa6e1b9ecf0a
LENOVO <eldar200715@gmail.com>
2024-09-12 17:26:46 +0300

    some fix changes

--- src/main/java/com/kabarxx/store_example/domain/CartItem.java
+++ src/main/java/com/kabarxx/store_example/domain/CartItem.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -25,6 +26,9 @@ public class CartItem {
     @ManyToOne
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
     private Cart cart;
+
+    @Column(name = "price")
+    private BigDecimal price;
 
     @Column(name = "quantity")
     private int quantity;
