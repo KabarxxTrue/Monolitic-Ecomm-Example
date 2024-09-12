379d6623d23f845b0a82b24495559957991ea91e
LENOVO <eldar200715@gmail.com>
2024-09-12 18:37:11 +0300

    some fixes

--- src/main/java/com/kabarxx/store_example/repositories/LikedProductRepository.java
+++ src/main/java/com/kabarxx/store_example/repositories/LikedProductRepository.java
@@ -1,13 +1,14 @@
 package com.kabarxx.store_example.repositories;
 
 import com.kabarxx.store_example.domain.LikedProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface LikedProductRepository {
+public interface LikedProductRepository extends JpaRepository<LikedProduct, Long> {
     List<LikedProduct> findByUserId(Long userId);
     Optional<LikedProduct> findByUserIdAndProductId(Long userId, Long productId);
 }
