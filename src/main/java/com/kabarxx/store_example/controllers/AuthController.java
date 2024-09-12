f3481505e3b4fe8d1566128c9d94771443d150d1
LENOVO <eldar200715@gmail.com>
2024-09-12 12:36:07 +0300

    Updated auth controller

---
+++ src/main/java/com/kabarxx/store_example/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.kabarxx.store_example.controllers;
+
+import com.kabarxx.store_example.domain.dto.user.SignInRequest;
+import com.kabarxx.store_example.domain.dto.user.SignUpRequest;
+import com.kabarxx.store_example.security.jwt.JwtAuthenticationResponse;
+import com.kabarxx.store_example.services.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
