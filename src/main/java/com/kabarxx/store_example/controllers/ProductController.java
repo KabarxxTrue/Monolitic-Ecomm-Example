34747e0ebd99cf012ebc09b3ae19f513ca98f18b
LENOVO <eldar200715@gmail.com>
2024-09-12 14:56:39 +0300

    added controller for product

---
+++ src/main/java/com/kabarxx/store_example/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.kabarxx.store_example.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+}
