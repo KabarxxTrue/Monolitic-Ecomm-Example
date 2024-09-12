f3e242648fc1267fa7659fa1a663339838d1372c
LENOVO <eldar200715@gmail.com>
2024-09-12 10:29:01 +0300

    some fix changes

--- src/main/java/com/kabarxx/store_example/security/CustomUserDetailsService.java
+++ src/main/java/com/kabarxx/store_example/security/CustomUserDetailsService.java
@@ -5,7 +5,9 @@ import com.kabarxx.store_example.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
