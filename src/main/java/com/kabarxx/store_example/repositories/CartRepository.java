491fa85f28bcf112a46dfc20c3ca6a77bc7a9218
LENOVO <eldar200715@gmail.com>
2024-09-12 17:14:19 +0300

    fixing

--- src/main/java/com/kabarxx/store_example/repositories/CartRepository.java
+++ src/main/java/com/kabarxx/store_example/repositories/CartRepository.java
@@ -1,11 +1,12 @@
 package com.kabarxx.store_example.repositories;
 
 import com.kabarxx.store_example.domain.Cart;
-import com.kabarxx.store_example.domain.dto.CartDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    CartDTO findCartByUserId(Long userId);
+    Optional<Cart> findCartByUserId(Long userId);
 }
