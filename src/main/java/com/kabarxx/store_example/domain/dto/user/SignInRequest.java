d3d64c3e3a535130cf83cfd0ef3b3ec1ae5d66c2
LENOVO <eldar200715@gmail.com>
2024-09-12 12:37:33 +0300

    renamed userDTO to sign

---
+++ src/main/java/com/kabarxx/store_example/domain/dto/user/SignInRequest.java
@@ -0,0 +1,23 @@
+package com.kabarxx.store_example.domain.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInRequest
+{
+    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @NotBlank(message = "Имя пользователя не может быть пустыми")
+    private String username;
+
+    @Size(min = 8, max = 255, message = "Длина пароля должна быть от 8 до 255 символов")
+    @NotBlank(message = "Пароль не может быть пустыми")
+    private String password;
+}
---
+++ src/main/java/com/kabarxx/store_example/domain/dto/user/SignUpRequest.java
@@ -0,0 +1,23 @@
+package com.kabarxx.store_example.domain.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Builder
+public class SignUpRequest
+{
+    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @NotBlank(message = "Имя пользователя не может быть пустыми")
+    private String username;
+
+    @Size(min = 5, max = 255, message = "Адрес электронной почты должен содержать от 5 до 255 символов")
+    @NotBlank(message = "Адрес электронной почты не может быть пустыми")
+    @Email(message = "Email адрес должен быть в формате user@example.com")
+    private String email;
+
+    @Size(max = 255, message = "Длина пароля должна быть не более 255 символов")
+    private String password;
+}
